1aacad3cdb15ed43354ea25e744b737b7bddc1e9
Jason Xie <jasonluxie@gmail.com>
2024-06-23 20:33:08 -0500

    grid being generated dynamically

--- src/components/GridContainer.tsx
+++ src/components/GridContainer.tsx
@@ -7,21 +7,35 @@ import GridNode from "./GridNode";
 // interface containerMetaProps {}
 
 const GridContainer = () => {
-  //   let verticalAxis = Math.floor(document.documentElement.clientHeight / 28);
-  //   let horizontalAxis = Math.floor(document.documentElement.clientWidth / 28);
+  //get viewport height and width
+  let verticalAxis = Math.floor(document.documentElement.clientHeight / 28);
+  let horizontalAxis = Math.floor(document.documentElement.clientWidth / 28);
   //   console.log("vertical", verticalAxis);
   //   console.log("horizontal", horizontalAxis);
   //   console.log("total cubes", verticalAxis * horizontalAxis);
-  const rowNodes: Array<JSX.Element> = [];
 
-  for (let j = 0; j < 10; j++) {
-      rowNodes.push(<GridNode x={j + 1} y={i + 1} key={`node${j}${i}}`}></GridNode>);
+  let column = [];
+  let x = 1;
+  for (let y = 0; y < verticalAxis; y++) {
+    //need to reset horizontal array each loop
+    const rowNodes: Array<JSX.Element> = [];
+    for (let x = 0; x < horizontalAxis; x++) {
+      rowNodes.push(
+        <GridNode x={x + 1} y={y + 1} key={`node${x}${y}}`}></GridNode>
+      );
     }
 
-
+    let row = (
+      <div className="flex" id={`row${x}`}>
+        {rowNodes.map((child) => child)}
+      </div>
+    );
+    column.push(row);
+    x++;
+  }
   return (
     <>
-      <div className={`h-svh w-svw flex`}>{rowNodes}</div>
+      <div className={`h-svh w-svw`}>{column.map((child) => child)}</div>
     </>
   );
 };
